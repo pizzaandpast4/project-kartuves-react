a73360537d8a16fd63b638be49488c03198311f3
pizzaandpast4 <dmitrij.fiodorov@gmail.com>
2024-08-17 14:51:04 +0300

    game

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Header } from './components/header/Header'
 import { Footer } from './components/footer/Footer'
 import { randomWord } from './components/wordgenerator/WordGenerator'
@@ -6,18 +6,33 @@ import './App.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [word, setWord] = useState('');
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongGuesses, setWrongGuesses] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
+  const [gameWon, setGameWon] = useState(false);
+  const [wins, setWins] = useState(0);
+  const [losses, setLosses] = useState(0);
+
+  useEffect(() => {
+    startNewGame();
+  }, []);
+
+  function startNewGame() {
+    setWord(randomWord);
+    setGuessedLetters([]);
+    setWrongGuesses(0);
+    setGameOver(false);
+    setGameWon(false);
+  }
 
   return (
     <>
       <Header />
-      <main className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          {randomWord}
-        </p>
+      <main>
+        <div>
+          {word}
+        </div>
       </main>
       <Footer />
     </>
